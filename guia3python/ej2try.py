bf2e371dd932d0189c91e2ba055d3bba49d8c389
joaquin Chayle <joacochayle@gmail.com>
2024-04-12 11:40:52 -0300

    subiendo archivo

---
+++ guia3python/ej2try.py
@@ -0,0 +1,8 @@
+try:
+    numero1 = int(input("Ingresa el primer número: "))
+    numero2 = int(input("Ingresa el segundo número: "))
+    division = numero1/numero2
+    print(f"La división de los dos numeros ingresados es: {division}")
+except ZeroDivisionError:
+    print("La division no es dividible por 0")
+    
